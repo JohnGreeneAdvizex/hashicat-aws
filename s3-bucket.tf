f1826ef21c81b16aee07949d33fde81db81fe5ae
John Greene <jgreene@advizex.com>
2021-03-18 19:14:36 +0000

    added s3 bucket  module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,5 +3,5 @@ use module and workspace outputs as inputs.
 module "s3-bucket" {
   source  = "app.terraform.io/JohnGreene-training/s3-bucket/aws"
   version = "1.15.0"
-  # insert required variables here
+  bucket_prefix = "john-greene"
 }
