ed5d285f5f158f56861150ef7270c6d5a8d5eeb2
JohnGreeneAdvizex <63053527+JohnGreeneAdvizex@users.noreply.github.com>
2021-03-18 15:09:10 -0400

    Create s3-bucket.tf

---
+++ s3-bucket.tf
@@ -0,0 +1,6 @@
+use module and workspace outputs as inputs.
+module "s3-bucket" {
+  source  = "app.terraform.io/JohnGreene-training/s3-bucket/aws"
+  version = "1.15.0"
+  # insert required variables here
+}
